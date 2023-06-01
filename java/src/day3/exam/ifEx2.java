236c633743dbfaee4974d6c7c13564bb3c9dab8b
Honey-Bi <biten10@naver.com>
2023-06-01 14:34:37 +0900

    홀짝

--- java/src/day3/exam/ifEx2.java
+++ java/src/day3/exam/ifEx2.java
@@ -2,13 +2,15 @@ package day3.exam;
 
 import java.util.Scanner;
 
-public class GradeEx {
+public class ifEx2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("input score : ");
-		int score = sc.nextInt();
-		System.out.println((score >= 60) ? "pass" : "fail");
+		
+		int num = sc.nextInt();
+		
+		System.out.println((num%2 == 1) ? "odd" : "even" );
+		
 		sc.close();
 	}
 
