ebff4dea751b9b0c5dcf6c317516d32c19cf4025
Honey-Bi <biten10@naver.com>
2023-06-02 12:39:08 +0900

    if 연습

--- java/src/day4/exam/IfEx2.java
+++ java/src/day4/exam/IfEx2.java
@@ -9,9 +9,9 @@ public class IfEx2 {
 		
 		int month = sc.nextInt();
 		
-		if (month == 3 || month == 4 || month == 5) System.out.println("Spring");
-		else if (month == 6 || month == 7 || month == 8) System.out.println("Summer");
-		else if (month == 9 || month == 10 || month == 11) System.out.println("Autumn");
+		if (month >= 3 && month <= 5) System.out.println("Spring");
+		else if (month >= 6&& month <= 8) System.out.println("Summer");
+		else if (month >= 9 && month <= 11) System.out.println("Autumn");
 		else if (month == 12 || month == 1 || month == 2) System.out.println("Winter");
 		else System.out.println("wrong month");
 		
---
+++ java/src/day4/exam/ifEx4.java
@@ -0,0 +1,20 @@
+package day4.exam;
+
+import java.util.Scanner;
+
+public class ifEx4 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int num = sc.nextInt();
+		
+		if (num % 6 == 0) System.out.println("multiple of 6");
+		else if (num % 3 == 0) System.out.println("multiple of 3");
+		else if (num % 2 == 0) System.out.println("multiple of 2");
+		else System.out.println("not multiple of 2, 3, 6");
+		
+		sc.close();
+	}
+}
