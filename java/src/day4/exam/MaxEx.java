87a7f09d0dfce6af2c2f319efeeca13d5a31d382
Honey-Bi <biten10@naver.com>
2023-06-02 14:56:31 +0900

    if로 max 계산

---
+++ java/src/day4/exam/MaxEx.java
@@ -0,0 +1,17 @@
+package day4.exam;
+
+import java.util.Scanner;
+
+public class MaxEx {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt(), num2 = sc.nextInt();
+		if(num < num2) System.out.println(num2);
+		else if (num > num2) System.out.println(num);
+		else System.out.println("equal");
+		
+		sc.close();
+	}		
+}
