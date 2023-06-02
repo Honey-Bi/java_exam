2376468c82e75077908e141ad23df499639c136c
Honey-Bi <biten10@naver.com>
2023-06-02 14:56:39 +0900

    if exam

---
+++ java/src/day4/exam/NumberEx.java
@@ -0,0 +1,18 @@
+package day4.exam;
+
+import java.util.Scanner;
+
+public class NumberEx {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		if (num % 2 == 0) num = num / 2;
+		else num++;
+		System.out.println(num);
+		
+		sc.close();
+
+	}
+}
