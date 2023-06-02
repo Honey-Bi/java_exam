d935134c5c10ebe9db30ec5b888ef5b7d715506a
Honey-Bi <biten10@naver.com>
2023-06-02 14:56:56 +0900

    if만 써서 삼각형 계산

---
+++ java/src/day4/exam/TriangleEx.java
@@ -0,0 +1,28 @@
+package day4.exam;
+
+import java.util.Scanner;
+
+public class TriangleEx {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+		
+		if (a > b && a > c) {
+			if (a < b + c) System.out.println(true);
+			else System.out.println(false);
+		} else if (b > a && b > c) {
+			if (b < a + c) System.out.println(true);
+			else System.out.println(false);
+		} else if (c > b && c > a) {
+			if (c < a + b) System.out.println(true);
+			else System.out.println(false);
+		} else if (a == b && b == c) {
+			System.out.println(true);
+		} else System.out.println(false);
+		
+		
+		sc.close();
+	}
+}
