2e056550c41fa64105bfd46d6dcf1c8de537825d
Honey-Bi <biten10@naver.com>
2023-06-05 11:20:06 +0900

    while 예제

---
+++ java/src/day5/exam/WhileEx1.java
@@ -0,0 +1,13 @@
+package day5.exam;
+
+public class WhileEx1 {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while(i < 10) {
+			System.out.println("Hello World " + i);
+			i++;
+		}
+		
+	}
+}
