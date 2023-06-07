bf6812004c1e098a7dec32f777ae9377e0347776
Honey-Bi <biten10@naver.com>
2023-06-07 14:44:42 +0900

    updown ex 2

---
+++ java/src/day6/exam/UpDown2.java
@@ -0,0 +1,30 @@
+package day6.exam;
+
+import java.util.Scanner;
+
+public class UpDown2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int i = 0;
+
+		do {
+			int random = (int)(Math.random() * (100 - 1 + 1) + 1);
+			while (i != random) {
+				i = sc.nextInt();
+				
+				if (i > random) {
+					System.out.println("Down");
+				} else if(i < random) {
+					System.out.println("Up");
+				}
+			} 
+			System.out.println("Good");
+			
+		} while (sc.next().charAt(0) == 'y') ;
+		
+		
+		sc.close();		
+	}
+}
