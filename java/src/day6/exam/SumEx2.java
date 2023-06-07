48ea11db26476486a8520a4e95b21e73a8a35aa8
Honey-Bi <biten10@naver.com>
2023-06-07 10:15:32 +0900

    sum

---
+++ java/src/day6/exam/SumEx2.java
@@ -0,0 +1,21 @@
+package day6.exam;
+
+import java.util.Scanner;
+
+public class SumEx2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int even_sum = 0, i = 0, num = sc.nextInt();
+		
+		while (++i <= num) {
+			even_sum += ++i;
+			System.out.println(i);
+		}
+		System.out.println(even_sum);
+		
+		
+		sc.close();
+	}
+}
